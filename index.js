b2a2a337c4cea1baf8751fdd11200666cb2200e4
Grunill <jgrunill@btinternet.com>
2022-01-27 15:08:34 +0000

    http.createServer

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const express = require('express');
-const app = express();
+// console.log(__filename);
+// console.log(__dirname);
 
-app.get("/", (req, res) => {
-    res.send("WORKING 2022!!!");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/'){
+        fs.readFile(path.join(__dirname,'index.html'), (err, data) => {
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        });
+    }
+    if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('About');
+        res.end();
+    }
+    if (req.url === '/videos'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('Videos');
+        res.end();
+    }
+    if (req.url === '/todo'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('ToDo');
+        res.end();
+    }
 });
 
-app.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5000);
